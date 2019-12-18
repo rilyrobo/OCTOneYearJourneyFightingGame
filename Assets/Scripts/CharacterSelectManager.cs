ce21e75a0625f230a1bf958126419f710cdfb7ee
Bit <bakerjobaker.baker40@gmail.com>
2019-12-18 02:31:30 -0800

    Fixed for relativity

--- Assets/Scripts/CharacterSelectManager.cs
+++ Assets/Scripts/CharacterSelectManager.cs
@@ -4,25 +4,26 @@ using UnityEngine;
 
 public class CharacterSelectManager : MonoBehaviour
 {
-    private float[] vertical = new float[] {-117f, -196f, -275f, -353f};
+    private int[] vertical = new int[] {-117, -39, 39, 117};
     private float[] horizontal = new float[] {-237f,-158f,-79f,0f,79f,158f,237f};
     public GameObject parent;
 
     private void Start() {
         var h = 0;
         var v = 0;
-        foreach (Transform child in parent.transform)
+        foreach (RectTransform child in parent.transform)
         {            
-            child.position = parent.transform.position + new Vector3(horizontal[h],vertical[v],0);
-            h++;
+            child.anchoredPosition = new Vector3(horizontal[h],vertical[v],0);
+            v++;
             if(v==4){
                 v=0;
-                
+                h++;
             }
             if(h==7){
-                h=0;
-                v++;
+                h=0; 
             }
+
+            // Debug.Log(child.anchoredPosition);
         }
     }
 }
