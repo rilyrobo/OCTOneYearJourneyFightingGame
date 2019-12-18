ad008edb0f115a16cec1d87c3c000977d6c10638
Bit <bakerjobaker.baker40@gmail.com>
2019-12-18 01:27:35 -0800

    started character menu

---
+++ Assets/Scripts/CharacterManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour
+{
+    private float update;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Debug.Log("Start");
+        // update = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // update += Time.deltaTime;
+        // if (update > 1.0f)
+        // {
+        //     update = 0.0f;
+        //     Debug.Log("Update");
+        // }
+    }
+
+    void Awake()
+    {
+        // Debug.Log("Awake");
+    }
+}
---
+++ Assets/Scripts/CharacterSelectManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelectManager : MonoBehaviour
+{
+    private float[] vertical = new float[] {-117f, -196f, -275f, -353f};
+    private float[] horizontal = new float[] {-237f,-158f,-79f,0f,79f,158f,237f};
+    public GameObject parent;
+
+    private void Start() {
+        var h = 0;
+        var v = 0;
+        foreach (Transform child in parent.transform)
+        {            
+            child.position = parent.transform.position + new Vector3(horizontal[h],vertical[v],0);
+            h++;
+            if(v==4){
+                v=0;
+                
+            }
+            if(h==7){
+                h=0;
+                v++;
+            }
+        }
+    }
+}
